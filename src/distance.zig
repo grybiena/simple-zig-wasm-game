a54344c4188211fa1a33cb50978a9bd9c7337dbd
grybiena <135348827+grybiena@users.noreply.github.com>
2025-04-22 21:19:17 +0100

    this isn't manhattan!?

--- src/distance.zig
+++ src/distance.zig
@@ -1,5 +1,5 @@
 const position = @import("position.zig");
 
 pub fn manhattan(from: position.XY, to: position.XY) u8 {
-    return @abs(from.x - to.x) + @abs(from.y + to.y);
+    return @abs(from.x - to.x) + @abs(from.y - to.y);
 }
